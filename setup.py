1c30cb0af0c4a3890247070b9db87759a72d914f
thebaptiste <jean-baptiste.veslin@meteo.fr>
2020-02-21 15:58:52 +0100

    build: fix again again

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools import find_packages
 with open('requirements.txt') as reqs:
     install_requires = [
         line for line in reqs.read().split('\n')
-        if (line and not line.startswith('--')) and (";" not in line)]
+        if (line and not line.startswith('--')) and (";" not in line)
+        and not line.startswith('git+https')]]
 
 setup(
     name='mfutil',
