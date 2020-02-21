3b67df4a9158a12b575883c3ef39d79cf07660bf
thebaptiste <jean-baptiste.veslin@meteo.fr>
2020-02-21 16:07:10 +0100

    feat: add possibility to exclude files when releasing a plugin
    About https://github.com/metwork-framework/mfdata/issues/241

--- setup.py
+++ setup.py
@@ -2,9 +2,16 @@ import fastentrypoints  # noqa: F401
 from setuptools import setup
 from setuptools import find_packages
 
+with open('requirements.txt') as reqs:
+    install_requires = [
+        line for line in reqs.read().split('\n')
+        if (line and not line.startswith('--')) and (";" not in line)
+        and not line.startswith('git+https')]
+
 setup(
     name='mfutil',
     packages=find_packages(),
+    install_requires=install_requires,
     entry_points={
         "console_scripts": [
             "get_ip_for_hostname = mfutil.cli_tools.get_ip_for_hostname:main",
