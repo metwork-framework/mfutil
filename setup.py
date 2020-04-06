dbe372f9996fe777effc0fcadfd36285966ef60f
MARTY FABIEN <fabien.marty@meteo.fr>
2020-04-06 12:18:22 +0200

    feat: remove plugins features (moved to mfplugin repository)

--- setup.py
+++ setup.py
@@ -38,16 +38,6 @@ setup(
             "get_full_hostname = mfutil.cli_tools.get_full_hostname:main",
             "get_domainname = mfutil.cli_tools.get_domainname:main",
             "get_real_ip = mfutil.cli_tools.get_real_ip:main",
-            "plugins.list = mfutil.cli_tools.plugins_list:main",
-            "plugins.validate_name = "
-            "mfutil.cli_tools.plugins_validate_name:main",
-            "plugins.info = mfutil.cli_tools.plugins_info:main",
-            "plugins.hash = mfutil.cli_tools.plugins_hash:main",
-            "_plugins.init = mfutil.cli_tools.plugins_init:main",
-            "_plugins.make = mfutil.cli_tools.plugins_make:main",
-            "_plugins.develop = mfutil.cli_tools.plugins_develop:main",
-            "plugins.install = mfutil.cli_tools.plugins_install:main",
-            "plugins.uninstall = mfutil.cli_tools.plugins_uninstall:main",
             "ping_tcp_port = mfutil.cli_tools.ping_tcp_port:main",
             "recursive_kill.py = mfutil.cli_tools.recursive_kill:main",
         ]
