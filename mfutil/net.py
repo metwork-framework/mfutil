1668250ae0ad41333de8c6824437de75209a7186
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-03-11 16:48:52 +0000

    feat: prepare the usage of netifaces2 instead of archived netifaces

--- mfutil/net.py
+++ mfutil/net.py
@@ -120,7 +120,12 @@ def _get_real_ip_netifaces():
     ip = "127.0.0.1"
     try:
         # we try first with the default gateway
-        def_gw_device = netifaces.gateways()['default'][netifaces.AF_INET][1]
+        if 'default_gateway' in dir(netifaces):
+            #netifaces2
+            def_gw_device = netifaces.default_gateway(old_api=True)[netifaces.AF_INET][1]
+        else:
+            #original netifaces
+            def_gw_device = netifaces.gateways()['default'][netifaces.AF_INET][1]
         infos = netifaces.ifaddresses(def_gw_device)
         ip = str(infos[netifaces.AF_INET][0]['addr'])
     except Exception:
